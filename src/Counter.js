992560a233369a18a8f851a2d2d19e0348e8292a
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-08-22 10:35:57 -0300

    Criando exemplo do UseEffect

--- src/Counter.js
+++ src/Counter.js
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import './Counter.css';
 
 const Counter = () => {
   const [contador, setCounter] = useState(0);
+  const [teste, setTeste] = useState(123);
+
+  useEffect(() => {
+    console.log('Testando useEffect');
+  }, [teste]);
+
   const handleOnClick = operador => {
     const newValue = operador === '+' ? contador + 1: contador - 1
 
@@ -16,6 +22,7 @@ const Counter = () => {
       <div>
         <button onClick={() => handleOnClick('-')}>-</button>
         <button onClick={() => handleOnClick('+')}>+</button>
+        <button onClick={() => setTeste(321)}>Teste</button>
       </div>
     </div>
   );
