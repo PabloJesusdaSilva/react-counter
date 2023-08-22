038c21a68c17e578a329a88567959a5423d36852
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-08-22 11:34:40 -0300

    Criando navegação simples

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Albums from "./Albums";
 import Counter from "./Counter";
 
+const defaultPage = ' albums';
+
+const pages = {
+  albums: {
+    text: 'Álbuns',
+    component: Albums
+  },
+  counter: {
+    text: 'Contador',
+    component: Counter
+  }
+}
+
 const App = () => {
+
+  const [page, setPage] = useState(defaultPage);
+
+  const handleChangePage = page => {
+    setPage(page);
+  }
+
+  const Page = pages[page].component;
+
+  const pageName = Object.keys(pages);
+
   return (
-    <Counter />
+    <>
+      {
+        pageName.map(page => <button onClick={() => handleChangePage(page)}> {pages[page].text} </button>)
+      }
+
+      { Page && <Page /> }
+    </>
   );
 }
 
